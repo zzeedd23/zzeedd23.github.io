af3321baad3b63451f9f72f7274cf363da17d5f8
zzeedd23 <115602741+zzeedd23@users.noreply.github.com>
2022-12-10 13:49:15 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,76 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact - Muhamad Shuro Fadhillah</title>
+    <!-- Favicon -->
+    
+    <!-- icons -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css">
+    <!-- Core theme CSS -->
+    <link rel="stylesheet" href="style.css">
+  </head>
+
+  <body>
+    <!-- Parallax Pixel Background Animation -->
+    <section class="animated-background">
+      <div id="stars1"></div>
+      <div id="stars2"></div>
+      <div id="stars3"></div>
+    </section>
+    <!-- End of Parallax Pixel Background Animation -->
+
+    <a id="profilePicture" href="#popup">
+      <img src="./img/profile.jpeg" alt="Profile Picture">
+    </a>
+
+    <div class="overlay" id="popup">
+      <div class="popup">
+        <div class="popup-photo">
+          <a href="./img/profile.jpeg" target="_blank">
+            <img src="./img/profile.jpeg" alt="Profile Picture">
+          </a>
+        </div>
+        <div class="popup-quote">
+          Moving on in despair is a very real pleasure, being crushed by circumstances is a joy
+        </div>
+        <a class="popup-close" onclick="history.back()">&times;</a>
+      </div>
+    </div>
+
+    <div id="userName">
+      <h2>Muhamad Shuro Fadhillah</h2>
+      <p>"Menjual semua hal yang pasti kamu butuhkan"</p>
+      <p>[ Middleman | Buaya | penjoki ]</p>
+    </div>
+
+    <div id="links">
+      <a class="link" href="https://m.me/Fdll14" target="_blank">
+        <i class="fab fa-facebook-messenger">&nbsp;</i>Chat Messenger (Muhamad Shuro Fadhillah)
+      </a>
+      <a class="link" href="https://www.facebook.com/Fdll14" target="_blank">
+        <i class="fab fa-facebook">&nbsp;</i>Facebook profile (Muhamad Shuro Fadhillah)
+      </a>
+      <a class="link" href="https://wa.me/62895422836123" target="_blank">
+        <i class="fab fa-whatsapp">&nbsp;</i>Whatsapp (Muhamad Shuro Fadhillah)
+      </a>
+      <br>
+      <br>
+      <!--<a class="link" href="https://wa.me/628988317458" target="_blank">-->
+      <!--  <i class="fab fa-whatsapp">&nbsp;</i>Whatsapp Bisnis (Shuro Store)-->
+      <!--</a>-->
+      <!--<a class="link" href="https://chat.whatsapp.com/KUfJWh1RiHe6tk1bL3BzB8" target="_blank">-->
+      <!--  <i class="fab fa-whatsapp">&nbsp;</i>Groups Whatsapp Buaya Cuan (JB)-->
+      <!--</a>-->
+      <!--<a class="link" href="https://chat.whatsapp.com/D95uXQayUY18cS6HP3qhzz" target="_blank">-->
+      <!--  <i class="fab fa-whatsapp">&nbsp;</i>Groups Whatsapp Jual Beli Email (JB)-->
+      <!--</a>-->
+    </div>
+
+    <div id="hashtag">
+      #Shuro Store ❤
+    </div>
+  </body>
+</html>
